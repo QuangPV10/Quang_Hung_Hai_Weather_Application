b4a821b6aeeeb4fd7695965d6dcd63220f539fb3
hungcb21 <hungcb21@gmail.com>
2022-01-18 14:02:36 +0700

    fix import

--- lib/src/blocs/bloc_dependencies.dart
+++ lib/src/blocs/bloc_dependencies.dart
@@ -1,10 +1,9 @@
 import 'package:flutter_simple_dependency_injection/injector.dart';
-
-import '../services/search_service/search_service.dart';
-import '../services/weather_service/weather_service.dart';
-import './current_weather_bloc/current_weather_bloc.dart';
-import './search_bloc/search_bloc.dart';
-import './week_forecast_weather_bloc/week_forecast_weather_bloc.dart';
+import 'package:quang_hung_hai_weather_application/src/blocs/current_weather_bloc/current_weather_bloc.dart';
+import 'package:quang_hung_hai_weather_application/src/blocs/search_bloc/search_bloc.dart';
+import 'package:quang_hung_hai_weather_application/src/blocs/week_forecast_weather_bloc/week_forecast_weather_bloc.dart';
+import 'package:quang_hung_hai_weather_application/src/services/search_service/search_service.dart';
+import 'package:quang_hung_hai_weather_application/src/services/weather_service/weather_service.dart';
 
 class BlocsDependencies {
   static Injector initialise(Injector injector) {
@@ -13,7 +12,7 @@ class BlocsDependencies {
             WeekForeCastWeatherBloc(service: injector.get<WeatherService>()),isSingleton: true);
     injector.map<CurrentWeatherBloc>(
         (injector) =>
-            CurrentWeatherBloc(service: injector.get<WeatherService>()),
+            CurrentWeatherBloc(service: injector.get<WeatherService>()  ),
         isSingleton: true);
     injector.map<SearchBloc>(
         (injector) => SearchBloc(service: injector.get<SearchService>()),
